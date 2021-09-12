3295165013128e0528f21b6397904f331f0c049d
Carlos Arthur Santili <74028702+CarlosArthurSantili@users.noreply.github.com>
2021-09-12 18:14:33 -0300

    Implementação do banco de dados

---
+++ e-Locadora5.DatabaseSQL/dbo/Tables/TBCondutor.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[TBCondutor] (
+    [Id]          INT          IDENTITY (1, 1) NOT NULL,
+    [Nome]        VARCHAR (50) NOT NULL,
+    [Endereco]    VARCHAR (80) NOT NULL,
+    [Telefone]    VARCHAR (20) NOT NULL,
+    [NumeroRg]    VARCHAR (15) NOT NULL,
+    [NumeroCpf]   VARCHAR (20) NOT NULL,
+    [NumeroCnh]   VARCHAR (30) NOT NULL,
+    [ValidadeCnh] DATE         NOT NULL,
+    [Id_Cliente]  INT          NOT NULL,
+    CONSTRAINT [PK_TBCondutor] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_TBCondutor_TBClientes] FOREIGN KEY ([Id_Cliente]) REFERENCES [dbo].[TBClientes] ([Id])
+);
+
