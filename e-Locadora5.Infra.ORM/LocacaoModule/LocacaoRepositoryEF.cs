15022339eee593f2e5bf0efacd6f150b77c78dba
Marcos Matos <marcoscaiobel@gmail.com>
2021-09-13 17:50:10 -0300

    infraLocacaoORMeDAO

---
+++ e-Locadora5.Infra.ORM/LocacaoModule/LocacaoRepositoryEF.cs
@@ -0,0 +1,18 @@
+﻿using e_Locadora5.Dominio.LocacaoModule;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace e_Locadora5.Infra.ORM.LocacaoModule
+{
+    public class LocacaoRepositoryEF : ILocacaoRepository
+
+    {
+        public void InserirLocacao(Locacao locacao)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
