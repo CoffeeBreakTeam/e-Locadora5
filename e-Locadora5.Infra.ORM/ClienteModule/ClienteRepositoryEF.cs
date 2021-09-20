f359f58dae29f4cbcb3c61a238323e8a3a39d54c
Marcos Matos <marcoscaiobel@gmail.com>
2021-09-20 14:04:53 -0300

    pacoteClienteRepositoryEF

---
+++ e-Locadora5.Infra.ORM/ClienteModule/ClienteRepositoryEF.cs
@@ -0,0 +1,42 @@
+﻿using e_Locadora5.Dominio.ClientesModule;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace e_Locadora5.Infra.ORM.ClienteModule
+{
+    public class ClienteRepositoryEF : IClienteRepository
+    {
+        public void EditarCliente(int id, Clientes cliente)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ExcluirCliente(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Existe(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InserirCliente(Clientes cliente)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Clientes SelecionarClientePorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Clientes> SelecionarTodosClientes()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
