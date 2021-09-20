1334fb8ba8f73fe5234a032b92c2729197b91e90
Marcos Matos <marcoscaiobel@gmail.com>
2021-09-20 16:27:19 -0300

    ProjetoDataBuilderTest

---
+++ e-Locadora5.DataBuilderTest/ClienteModule/ClienteDataBuilder.cs
@@ -0,0 +1,58 @@
+﻿using e_Locadora5.Dominio.ClientesModule;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace e_Locadora5.DataBuilderTest.ClienteModule
+{
+    [TestClass]
+    public class  ClienteDataBuilder
+    {
+        private Clientes clientes;
+        public Clientes Build()
+        {
+            return clientes;
+        }
+        public ClienteDataBuilder()
+        {
+            clientes = new Clientes();
+        }
+        public ClienteDataBuilder ComNome(string nome)
+        {
+            clientes.Nome = nome;
+            return this;
+        }
+        public ClienteDataBuilder ComEndereco(string endereco)
+        {
+            clientes.Endereco = endereco;
+            return this;
+        }
+        public ClienteDataBuilder ComTelefone(string telefone)
+        {
+            clientes.Telefone = telefone;
+            return this;
+        }
+        public ClienteDataBuilder ComRG(string RG)
+        {
+            clientes.RG = RG;
+            return this;
+        }
+        public ClienteDataBuilder ComCPF(string CPF)
+        {
+            clientes.CPF = CPF;
+            return this;
+        }
+        public ClienteDataBuilder ComCNPJ(string CNPJ)
+        {
+            clientes.CNPJ = CNPJ;
+            return this;
+        }
+        public ClienteDataBuilder ComEmail(string email)
+        {
+            clientes.Email = email;
+            return this;
+        }
+    }
+}
