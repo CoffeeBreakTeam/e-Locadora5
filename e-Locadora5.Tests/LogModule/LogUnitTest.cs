5e40489ccc608985b91a81c6985ac1051ed6a7eb
johnboeira <jbiel72001@gmail.com>
2021-09-24 15:44:00 -0300

    classe log e metodos prontos

---
+++ e-Locadora5.Tests/LogModule/LogUnitTest.cs
@@ -0,0 +1,39 @@
+﻿
+using e_Locadora5.Infra.Log;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace e_Locadora5.Tests.LogModule
+{
+    [TestClass]
+    public class LogUnitTest
+    {
+        GeradorDeLog geradorDeLog;
+        public LogUnitTest()
+        {
+            geradorDeLog = new GeradorDeLog();
+        }
+
+        [TestMethod]
+        public void EnviandoLogParaSeq()
+        {
+            //arrange
+            string mensagem = "Tela abc aberta";
+            //act
+            var registrou = geradorDeLog.RegistrarLog(mensagem, NiveisSerilog.Information);
+            //assert
+            registrou.Should().Be(true);
+        }
+
+        [TestMethod]
+        public void GravandoLogEmTxt()
+        {          
+            //arrange
+            string mensagem = "Tela abc aberta salvando em txt";
+            //act
+            var registrou = geradorDeLog.RegistrarLog(mensagem, NiveisSerilog.Information);
+            //assert
+            registrou.Should().Be(true);
+        }
+    }
+}
