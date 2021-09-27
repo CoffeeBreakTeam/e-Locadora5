4766260c47da5819eafce6277e42c830365be10e
Carlos Arthur Santili <74028702+CarlosArthurSantili@users.noreply.github.com>
2021-09-27 15:43:50 -0300

    Recriação do projeto de Logs, devido a conflito de namespaces

---
+++ e-Locadora5.Infra.GeradorLogs/GeradorDeLog.cs
@@ -0,0 +1,20 @@
+﻿using Serilog;
+using Serilog.Core;
+using System;
+
+namespace e_Locadora5.Infra.GeradorLogs
+{
+    public static class GeradorDeLog
+    {
+
+        public static void ConfigurarLog()
+        {
+            Logger logger = new LoggerConfiguration()
+               .WriteTo.Seq("http://localhost:5341")
+               .WriteTo.File("C:\\Users\\Cliente\\Desktop\\Locadora\\e-Locadora5\\e-Locadora5.Infra.Log\\bin\\Debug\\net5.0\\log-.txt", rollingInterval: RollingInterval.Day)
+               .CreateLogger();
+            Serilog.Log.Logger = logger;
+        }
+
+    }
+}
