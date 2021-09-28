3e397d4004a99eed597dd174a365f2bccd86c7f6
marcosmatos1203 <marcoscaiobel@gmail.com>
2021-09-28 16:31:10 -0300

    Log grupoDeVeiculoAppService

--- e-Locadora5.Aplicacao/GrupoVeiculoModule/GrupoVeiculoAppService.cs
+++ e-Locadora5.Aplicacao/GrupoVeiculoModule/GrupoVeiculoAppService.cs
@@ -1,5 +1,6 @@
 ﻿using e_Locadora5.Dominio;
 using e_Locadora5.Dominio.GrupoVeiculoModule;
+using Serilog;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,26 +20,59 @@ namespace e_Locadora5.Aplicacao.GrupoVeiculoModule
 
         public string InserirNovo(GrupoVeiculo registro)
         {
-            string resultadoValidacao = registro.Validar();
 
-            if (resultadoValidacao == "ESTA_VALIDO")
+            try
             {
-                grupoVeiculoRepository.InserirNovo(registro);
+                string resultadoValidacao = registro.Validar();
+
+                if (resultadoValidacao == "ESTA_VALIDO")
+                {
+                    grupoVeiculoRepository.InserirNovo(registro);
+                    Log.Information("Grupo de veiculo {@grupo} foi inserido com sucesso.", registro);
+                    return "ESTA_VALIDO";
+                }
+                else
+                {
+                    Log.Warning("Grupo de veiculo {@grupo} inválida: {@resultadoValidacao}", registro, resultadoValidacao);
+                    return resultadoValidacao;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "Não foi possível inserir o grupo {@grupo}", registro);
+                return "Não foi possível inserir o grupo de veículo";
             }
 
-            return resultadoValidacao;
         }
 
         public string Editar(int id, GrupoVeiculo registro)
         {
-            string resultadoValidacao = registro.Validar();
-
-            if (resultadoValidacao == "ESTA_VALIDO")
+            try
             {
-                grupoVeiculoRepository.Editar(id, registro);
+                string resultadoValidacao = registro.Validar();
+
+
+                if (resultadoValidacao == "ESTA_VALIDO")
+                {
+                    grupoVeiculoRepository.Editar(id, registro);
+                    Log.Information("Grupo {@grupo} foi editado com sucesso.", registro);
+                    return "ESTA_VALIDO";
+                }
+
+                else
+                {
+                    Log.Warning("Grupo {@grupo} inválida: {@resultadoValidacao}", registro, resultadoValidacao);
+                    return resultadoValidacao;
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "Não foi possível editar a grupo {@grupo}", registro);
+                return "Não foi possível editar o grupo";
             }
 
-            return resultadoValidacao;
+
         }
 
         public bool Excluir(int id)
@@ -46,28 +80,59 @@ namespace e_Locadora5.Aplicacao.GrupoVeiculoModule
             try
             {
                 grupoVeiculoRepository.Excluir(id);
+                Log.Information("TaxaServico de id {@id} foi excluído com sucesso", id);
+                return true;
             }
-            catch (Exception)
+            catch (Exception ex)
             {
+                Log.Error(ex, "Não foi possível excluir o grupo com id {@id}", id);
                 return false;
             }
-
-            return true;
         }
 
         public bool Existe(int id)
         {
-            return grupoVeiculoRepository.Existe(id);
+            try
+            {
+                bool existe = grupoVeiculoRepository.Existe(id);
+                Log.Information("Verificado se existe o grupo com id {@id}", id);
+                return existe;
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "Não foi possível verificar se existe o grupo com id {@id}", id);
+                return false;
+            }
         }
 
         public GrupoVeiculo SelecionarPorId(int id)
         {
-            return grupoVeiculoRepository.SelecionarPorId(id);
+            try
+            {
+                GrupoVeiculo grupoSelecionado = grupoVeiculoRepository.SelecionarPorId(id);
+                Log.Information("Selecionado grupo {@grupo}", grupoSelecionado);
+                return grupoSelecionado;
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "Não foi possível selecionar o grupo com id {@id}", id);
+                return null;
+            }
         }
 
         public List<GrupoVeiculo> SelecionarTodos()
         {
-            return grupoVeiculoRepository.SelecionarTodos();
+            try
+            {
+                List<GrupoVeiculo> listaGrupos = grupoVeiculoRepository.SelecionarTodos();
+                Log.Information("Selecionado todas os grupos {@todos}", listaGrupos);
+                return listaGrupos;
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "Não foi possível selecionar todos os grupos");
+                return null;
+            }
         }
     }
 }
