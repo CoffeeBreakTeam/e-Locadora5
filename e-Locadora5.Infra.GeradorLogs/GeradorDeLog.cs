84c4f4ad6fac384c648d81b5eaba7908f71e4ed8
johnboeira <jbiel72001@gmail.com>
2021-09-28 17:53:10 -0300

    pequeno ajuste em GeradorDeLog

--- e-Locadora5.Infra.GeradorLogs/GeradorDeLog.cs
+++ e-Locadora5.Infra.GeradorLogs/GeradorDeLog.cs
@@ -12,11 +12,9 @@ namespace e_Locadora5.Infra.GeradorLogs
         public static void ConfigurarLog()
         {
             Logger logger = new LoggerConfiguration()
-               .WriteTo.Seq("http://20.206.137.196:5341")
-               .WriteTo.File(Directory.GetCurrentDirectory(), rollingInterval: RollingInterval.Day)
-               .WriteTo.Seq("http://localhost:5341")
                .Enrich.WithExceptionDetails()
-               .WriteTo.File("C:\\Users\\Cliente\\Desktop\\Locadora\\e-Locadora5\\e-Locadora5.Infra.Log\\bin\\Debug\\net5.0\\log-.txt", rollingInterval: RollingInterval.Day)
+               .WriteTo.Seq("http://20.206.137.196:5341")
+               .WriteTo.File(Directory.GetCurrentDirectory(), rollingInterval: RollingInterval.Day)                            
                .CreateLogger();
             Serilog.Log.Logger = logger;
         }
