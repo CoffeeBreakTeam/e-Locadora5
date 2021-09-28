638045c794ed01ea4b45a7371c511850c5ed112d
Carlos Arthur Santili <74028702+CarlosArthurSantili@users.noreply.github.com>
2021-09-28 14:37:00 -0300

    Implementado server azure e alterado endereço para qual os logs são enviados

--- e-Locadora5.Infra.GeradorLogs/GeradorDeLog.cs
+++ e-Locadora5.Infra.GeradorLogs/GeradorDeLog.cs
@@ -10,7 +10,7 @@ namespace e_Locadora5.Infra.GeradorLogs
         public static void ConfigurarLog()
         {
             Logger logger = new LoggerConfiguration()
-               .WriteTo.Seq("http://localhost:5341")
+               .WriteTo.Seq("http://20.206.137.196:5341")
                .WriteTo.File("C:\\Users\\Cliente\\Desktop\\Locadora\\e-Locadora5\\e-Locadora5.Infra.Log\\bin\\Debug\\net5.0\\log-.txt", rollingInterval: RollingInterval.Day)
                .CreateLogger();
             Serilog.Log.Logger = logger;
