cc5b9ee056ec40799ed49039a975b86a2d4134c5
marcosmatos1203 <marcoscaiobel@gmail.com>
2021-10-08 13:55:05 -0300

    test veiculo

---
+++ e-Locadora5.EFTests/VeiculoModule/VeiculoEFTest.cs
@@ -0,0 +1,22 @@
+﻿using e_Locadora5.Aplicacao.GrupoVeiculoModule;
+using e_Locadora5.Aplicacao.VeiculoModule;
+using e_Locadora5.Dominio;
+using e_Locadora5.Dominio.GrupoVeiculoModule;
+using e_Locadora5.Dominio.VeiculosModule;
+using e_Locadora5.Infra.ORM.ParceiroModule;
+using e_Locadora5.Infra.ORM.VeiculoModule;
+using e_Locadora5.Infra.SQL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace e_Locadora5.EFTests.VeiculoModule
+{
+    public class VeiculoEFTest
+    {
+        
+    }
+}
