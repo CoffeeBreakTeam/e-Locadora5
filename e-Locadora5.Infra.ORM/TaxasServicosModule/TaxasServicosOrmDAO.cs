15f4d0949686aad4da6fab9be72a88ab8afd4d25
johnboeira <jbiel72001@gmail.com>
2021-10-08 14:09:34 -0300

    criado test e orm de TaxasServicos

---
+++ e-Locadora5.Infra.ORM/TaxasServicosModule/TaxasServicosOrmDAO.cs
@@ -0,0 +1,19 @@
+﻿using e_Locadora5.Dominio.TaxasServicosModule;
+using e_Locadora5.Infra.ORM.LocadoraModule;
+using e_Locadora5.Infra.ORM.ParceiroModule;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace e_Locadora5.Infra.ORM.TaxasServicosModule
+{
+    public class TaxasServicosOrmDAO : RepositoryBase<TaxasServicos, int>, ITaxasServicosRepository
+
+    {
+        public TaxasServicosOrmDAO(LocadoraDbContext locadoraDbContext) : base(locadoraDbContext)
+        {
+        }
+    }
+}
