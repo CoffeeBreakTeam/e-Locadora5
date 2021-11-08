1de207aefafcd5488a7e697de6e595c53aa9c4f5
johnboeira <jbiel72001@gmail.com>
2021-11-07 23:37:27 -0300

    parceiroViewModel

---
+++ e-Locadora5.WebApplication/ViewModels/ParceiroViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace e_Locadora5.WebApplication.ViewModels
+{
+    public class ParceiroViewModel
+    {
+
+        #region list
+
+
+        #endregion
+    }
+}
